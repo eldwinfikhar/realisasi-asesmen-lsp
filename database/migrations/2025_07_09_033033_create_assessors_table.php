8ea401c077631a222317e15e94ffef0f36866f6c
Eldwin Fikhar Ananda <eldwin.fikhar231104@gmail.com>
2025-08-21 08:12:22 +0700

    fix: merevisi migrasi untuk model Assessor

--- database/migrations/2025_07_09_033033_create_assessors_table.php
+++ database/migrations/2025_07_09_033033_create_assessors_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('assessors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('registration_number')->unique();
             $table->timestamps();
         });
     }
